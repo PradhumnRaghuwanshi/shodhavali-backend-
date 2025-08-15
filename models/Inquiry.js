874f06725e437d135cc5588a2c97b2ec0550c4f9
PradhumnRaghuwanshi <124507721+PradhumnRaghuwanshi@users.noreply.github.com>
2025-08-15 06:06:05 +0000

    text msg

--- models/Inquiry.js
+++ models/Inquiry.js
@@ -1,9 +1,17 @@
-const mongoose =  require("mongoose");
+const mongoose = require("mongoose");
 
 const inquirySchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  message: { type: String, required: true },
+  phone: { type: String, required: true },
+  affiliation: { type: String },
+  address: { type: String},
+  title: { type: String },
+  abstract: { type: String },
+
+  // We'll store the manuscript as a file path or URL
+  manuscriptPath: { type: String }, // e.g., '/uploads/manuscript.docx'
+
   date: { type: Date, default: Date.now }
 });
 
